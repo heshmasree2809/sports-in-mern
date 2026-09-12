4ee09b89f80d2405a3495171b9ed157493c47639
Heshma Sree <avuthuheshmasree@gmail.com>
2026-09-12 19:01:02 +0530

    Fix catch block formatting in booking system

--- Online Booking Streamline Reservation Systems.js
+++ Online Booking Streamline Reservation Systems.js
@@ -11,7 +11,8 @@ app.post('/book', async (req, res) => {
         const booking = new Booking({ facility, user, date, timeSlot });
         await booking.save();
         res.status(200).send('Booking successful');
-    } catch (error) 
+    } 
+    catch (error) 
     {
         res.status(500).send(error.message);
     }
