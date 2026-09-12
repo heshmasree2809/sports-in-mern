df217c994abd3ff76fb08485cc558ed331de9f83
Heshma Sree <avuthuheshmasree@gmail.com>
2026-09-12 19:00:19 +0530

    Fix formatting of object destructuring in booking route

--- Online Booking Streamline Reservation Systems.js
+++ Online Booking Streamline Reservation Systems.js
@@ -1,7 +1,8 @@
 // Booking a facility
 app.post('/book', async (req, res) => {
     try {
-        const { facility, user, date, timeSlot } = req.body;
+        const 
+        {  facility, user, date, timeSlot } = req.body;
         // Validate input
         if (!facility || !user || !date || !timeSlot) {
             return res.status(400).send('All booking details are required.');
