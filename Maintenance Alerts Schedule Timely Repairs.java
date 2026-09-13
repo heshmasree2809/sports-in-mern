c385028d4f9bc87d5b26953e90eaf704c0111ac2
Heshma Sree <avuthuheshmasree@gmail.com>
2026-09-13 19:40:00 +0530

    Fix catch block formatting in maintenance alert handler

--- Maintenance Alerts Schedule Timely Repairs.java
+++ Maintenance Alerts Schedule Timely Repairs.java
@@ -13,7 +13,8 @@ app.post('/maintenance-alert', async (req, res) => {
         });
         await maintenance.save();
         res.status(200).json({ message: 'Maintenance alert created', maintenance });
-    } catch (error)
+    } 
+catch (error)
     {
         res.status(500).send(error.message);
     }
