27b4aac10a94fe1a915cb75cf82c846afd5b0d36
Heshma Sree <avuthuheshmasree@gmail.com>
2026-09-13 19:44:07 +0530

    Fix indentation in error handling block

--- Online Booking Streamline Reservation Systems.js
+++ Online Booking Streamline Reservation Systems.js
@@ -21,7 +21,8 @@ app.get('/bookings',async (req, res) => {
     {
         const bookings=await Booking.find();
         res.status(200).json(bookings);
-    } catch (error) 
+    } 
+    catch (error) 
     {
         res.status(500).send(error.message);
     }
